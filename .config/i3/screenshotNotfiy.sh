140bf24e77cfc2068b80926af9437978638ca5de
yymirr <abla.ue@web.de>
2018-09-01 23:28:18 +0200

    changed notification border color, fixed screenshot scripts

--- .config/i3/screenshotNotfiy.sh
+++ .config/i3/screenshotNotfiy.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 
-scrot -s ~/media/screenshots/shot-$(date '+%d-%b-%Y-%H-%M').png -q 100 -e 'xclip-selection c -t image/png < $f' && notify-send -u critical "Screnshot" "Image located at ~/media/screenshots"
+scrot -s ~/media/screenshots/shot-$(date '+%d-%b-%Y-%H-%M').png -q 100 -e 'xclip-selection
+c -t image/png < $f' && notify-send -u critical "Screenshot successful!" "Image located at ~/media/screenshots"
 
