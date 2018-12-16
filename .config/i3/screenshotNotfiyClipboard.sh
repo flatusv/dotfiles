efa6a43ebca61dc93fd06e4cdbba9892aa39a868
yymirr <abla.ue@web.de>
2018-12-16 23:06:49 +0100

    set icon path for dunst, screenshot notification comes with an icon

--- .config/i3/screenshotNotfiyClipboard.sh
+++ .config/i3/screenshotNotfiyClipboard.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 scrot -s /tmp/screenshot-$(date '+%d-%b-%Y_%H-%M').png -e 'xclip -selection clipboard -t "image/png" < $f'
-notify-send -u critical "Screenshot successful!" "Image copied to clipboard"
+notify-send -i checkbox-checked -u critical "Screenshot successful!" "Image copied to clipboard"
 
