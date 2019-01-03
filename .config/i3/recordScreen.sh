90fd07fd1b3bf974ab78c0ccc2025383d6b85d43
yymirr <abla.ue@web.de>
2019-01-03 15:42:25 +0100

    remove stupid file

--- .config/i3/recordScreen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dunstify -r 111 "Screen capture pending" "press C-r to toggle screen recording" 
-simplescreenrecorder   --no-systray  --start-hidden && dunstify -r 111 "Success" "Video located at ~/media/screenshots/recorded" 
-
