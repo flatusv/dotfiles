185e4c6742fed4307baad6b157f229151a9ba295
yymirr <abla.ue@web.de>
2019-01-30 23:10:28 +0100

    changes to dunst and screenshot notification

--- .config/i3/screenshotNotfiy.sh
+++ .config/i3/screenshotNotfiy.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 scrot -s ~/media/screenshots/shot-$(date '+%d-%b-%Y-%H-%M').png -q 100 -e 'xclip-selection
-c -t image/png < $f' && notify-send -i checkbox-checked -u critical "Screenshot successful!" "Image located at ~/media/screenshots"
+c -t image/png < $f' && notify-send -i $HOME/media/wallpapers/icons/camera.png -u critical "Screenshot successful!" "Image located at ~/media/screenshots"
 
--- .config/i3/screenshotNotfiyClipboard.sh
+++ .config/i3/screenshotNotfiyClipboard.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 scrot -s /tmp/screenshot-$(date '+%d-%b-%Y_%H-%M').png -e 'xclip -selection clipboard -t "image/png" < $f'
-notify-send -i checkbox-checked -u critical "Screenshot successful!" "Image copied to clipboard"
+notify-send -i $HOME/media/wallpapers/icons/camera.png -u critical "Screenshot successful!" "Image copied to clipboard"
 
