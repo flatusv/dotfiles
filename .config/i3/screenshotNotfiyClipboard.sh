972d23155689cc2d5cd9893c1a672d77fc8b508b
yymirr <abla.ue@web.de>
2019-02-19 23:02:58 +0100

    changed appearance of notifications

--- .config/i3/screenshotNotfiyClipboard.sh
+++ .config/i3/screenshotNotfiyClipboard.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 scrot -s /tmp/screenshot-$(date '+%d-%b-%Y_%H-%M').png -e 'xclip -selection clipboard -t "image/png" < $f'
-notify-send -i $HOME/media/wallpapers/icons/camera.png -u critical "Screenshot successful!" "Image copied to clipboard"
+notify-send -i $HOME/media/wallpapers/icons/cute.png -u critical "<b>Screenshot successful!</b>" "Image copied to clipboard"
 
