a8b620bc3e0773c9456f3d3d8d0a0378d27d8f00
yymirr <abla.ue@web.de>
2019-04-11 11:48:47 +0200

    fixed notification behavior not showing bold text, changed background color

--- .config/i3/screenshotNotfiy.sh
+++ .config/i3/screenshotNotfiy.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 scrot -s ~/media/screenshots/shot-$(date '+%d-%b-%Y-%H-%M').png -q 100 -e 'xclip-selection
-c -t image/png < $f' && notify-send -i $HOME/media/wallpapers/icons/cute.png -u critical "<b>Screenshot successful!</b>" "Image located at ~/media/screenshots"
+c -t image/png < $f' && notify-send -i $HOME/media/wallpapers/icons/cute.png -u critical "Screenshot successful!" "Image located at ~/media/screenshots"
 
