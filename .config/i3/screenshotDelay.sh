e469abb2706f42a0f2025b1e9e2e86b58fe45b40
yymirr <abla.ue@web.de>
2019-07-28 20:30:03 +0200

    take screenshots of entire screen with a delay of 5 seconds

---
+++ .config/i3/screenshotDelay.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+scrot -d 5 -c ~/media/screenshots/shot-$(date '+%d-%b-%Y-%H-%M').png -q 100 -e 'xclip-selection
+c -t image/png < $f' && notify-send -i $HOME/media/wallpapers/icons/cute.png -u critical "Screenshot successful!" "Image located at ~/media/screenshots"
+
