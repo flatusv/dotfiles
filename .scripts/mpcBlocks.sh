23c087a3878b13ad8651810194c1a894ef370554
uestueab <abla.ue@web.de>
2021-05-20 17:51:09 +0200

    i3blocks mpv on steroids

--- .scripts/mpcBlocks.sh
+++ .scripts/mpcBlocks.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 if  [ "$(playerctl status)" == "Playing" ]
-   then
-       echo "mpv: <span foreground='#dccdfb'><b>$(playerctl metadata --format "{{ artist }} {{ title }}" | sed 's/2020-.*:[0-9]\+$//')</b></span>"
+then
+    currentTitle=$(playerctl metadata --format "{{ artist }} - {{ title }}" | sed 's/2020-.*:[0-9]\+$//')
+    echo "mpv: <span foreground='#dccdfb'><b>$currentTitle</b></span>"
 fi
+
+echo -e "\n"
+# background color within border
+echo -e "#2b2b2b"
+# border color
+echo -e "#9180e8"
 
