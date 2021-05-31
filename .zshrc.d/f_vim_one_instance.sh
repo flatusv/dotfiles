1bd056a7b3a5dfdf52d34a4c3ee6200cd17ff6d0
uestueab <abla.ue@web.de>
2021-05-31 14:26:34 +0200

    vim now handles spaces in filepaths

--- .zshrc.d/f_vim_one_instance.sh
+++ .zshrc.d/f_vim_one_instance.sh
@@ -1,6 +1,9 @@
 #/bin/bash
 
+
 function f_vim_one_instance() {
+
+    file="'$*'"
 
     #Vim is assigned to its specified workspace
     if ! pgrep -x "vim"; then 
@@ -13,14 +16,14 @@ function f_vim_one_instance() {
         nohup alacritty \
             --config-file=/home/geeray/.config/alacritty/alacritty-nogap.yml \
             -e sh \
-            -c "command vim --servername $(command vim --serverlist | head -1)  --remote-silent "$@"" >/dev/null 2>&1 &
+            -c "command vim --servername $(command vim --serverlist | head -1) --remote-silent $file" >/dev/null 2>&1 &
         exit
 
     else 
         nohup alacritty \
             --config-file=/home/geeray/.config/alacritty/alacritty-nogap.yml \
             -e sh \
-            -c "command vim --servername $(command vim --serverlist | head -1) --remote-silent "$@"" >/dev/null 2>&1 &
+            -c "command vim --servername $(command vim --serverlist | head -1) --remote-silent $file" >/dev/null 2>&1 &
         i3-msg 'workspace " 3:vim "' > /dev/null 2>&1   #switch to workspace
         exit
     fi 
