4067bcd33fa34de7b5c38ff9c115948f324d6192
uestueab <abla.ue@web.de>
2021-06-02 14:28:00 +0200

    editing vim hides the calling terminal rather than closing it, also switched back to the ws

--- .zshrc.d/f_vim_one_instance.sh
+++ .zshrc.d/f_vim_one_instance.sh
@@ -7,17 +7,36 @@ function f_vim_one_instance() {
 
     #Vim is assigned to its specified workspace
     if ! pgrep -x "vim"; then 
-        i3-msg 'move container to workspace number " 3:vim "' > /dev/null 2>&1
-        i3-msg 'workspace " 3:vim "' > /dev/null 2>&1   #switch to workspace
+        #i3-msg 'move container to workspace number " 3:vim "' > /dev/null 2>&1
+
+        #get current workspacw
+        CWS=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name')
+
+        #disappear calling terminal
+        i3-msg 'move scratchpad' >/dev/null 2>&1
+
+        #open vim in ws3
+        i3-msg 'workspace " 3:vim "' > /dev/null 2>&1
 
         #Open the file in a new terminal instance, witch a different config and detach
         #from calling terminal
-
         nohup alacritty \
             --config-file=/home/geeray/.config/alacritty/alacritty-nogap.yml \
             -e sh \
             -c "command vim --servername $(command vim --serverlist | head -1) --remote-silent $file" >/dev/null 2>&1 &
-        exit
+
+        # wait for vim process to finish
+        wait
+
+        #switch to the orginial workspace
+        i3-msg "workspace $CWS" >/dev/null 2>&1
+        #make calling terminal appear again
+        i3-msg "scratchpad show" >/dev/null 2>&1
+        #maximize it on screen
+        i3-msg "floating disable" >/dev/null 2>&1
+
+
+        #exit
 
     else 
         nohup alacritty \
@@ -28,4 +47,5 @@ function f_vim_one_instance() {
         exit
     fi 
 }
+
 
