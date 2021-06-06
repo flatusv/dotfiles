9ad541bb41484910bf0faaa4afc77f2e32bd5518
uestueab <abla.ue@web.de>
2021-06-06 11:48:50 +0200

    script no longer produces unwanted files, exits after layout cycle

--- .scripts/switchKeyboard.sh
+++ .scripts/switchKeyboard.sh
@@ -9,8 +9,9 @@ if grep -q "default" /tmp/kbdLayout.txt; then
     setxkbmap -option '' "vim"
 
     #let our script now, that we are using xkb
-    echo "xkb" >! /tmp/kbdLayout.txt
+    echo "xkb" >| /tmp/kbdLayout.txt
     echo "[SwitchKeyboard]: Using xkb layout!"
+    exit
 fi
 
 #switch to xmodmap (default) if xkb was detected
@@ -22,7 +23,8 @@ if grep -q "xkb" /tmp/kbdLayout.txt; then
     xmodmap ~/.Xmodmap
 
     #let our script now, that we are using xkb
-    echo "default" >! /tmp/kbdLayout.txt
+    echo "default" >| /tmp/kbdLayout.txt
     echo "[SwitchKeyboard]: Using xmodmap layout!"
+    exit
 fi
 
