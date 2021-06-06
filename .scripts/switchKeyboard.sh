e19e315c9e9d07e5716915b670fbeff7a8e9e3fe
uestueab <abla.ue@web.de>
2021-06-06 11:39:33 +0200

    cycle between xmodmap and xkb

--- .scripts/switchKeyboard.sh
+++ .scripts/switchKeyboard.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/bash
 
-CHOICE=$(echo -e "xmodmap: (default)\nxkb: (vim-like navigation)" | rofi -dmenu -p "Choose Layout > ")
-echo $choice
-
-
-#check if the selection was the default layout (xmodmap)
-if echo "$CHOICE" | grep -q "default"; then
+#cycle between xmodmap and xkb, depending on which is currently in effect
+if grep -q "default" /tmp/kbdLayout.txt; then
     #reset the current layout
     setxkbmap us
+    
+    #change to xkb
+    setxkbmap -option '' "vim"
+
+    #let our script now, that we are using xkb
+    echo "xkb" >! /tmp/kbdLayout.txt
+    echo "[SwitchKeyboard]: Using xkb layout!"
+fi
+
+#switch to xmodmap (default) if xkb was detected
+if grep -q "xkb" /tmp/kbdLayout.txt; then
+    #reset the current layout
+    setxkbmap us
+
     #then switch to xmodmap
     xmodmap ~/.Xmodmap
-else
-    #reset the current layout
-    setxkbmap us
-    setxkbmap -option '' "vim"
+
+    #let our script now, that we are using xkb
+    echo "default" >! /tmp/kbdLayout.txt
+    echo "[SwitchKeyboard]: Using xmodmap layout!"
 fi
 
