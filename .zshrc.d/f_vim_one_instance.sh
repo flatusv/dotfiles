18b9a60c66e7e8ee242e00f28e4af4b0cb2422ea
uestueab <abla.ue@web.de>
2021-06-06 14:38:16 +0200

    [FIX]: launch vim correctly, without specifying a file

--- .zshrc.d/f_vim_one_instance.sh
+++ .zshrc.d/f_vim_one_instance.sh
@@ -3,23 +3,37 @@
 
 function f_vim_one_instance() {
 
+    #[ATTENTION]: This fixes spaces in filenames!
+    # but makes it impossible to launch vim with more than one file
     file="'$*'"
 
-    #Vim is assigned to its specified workspace
-    if ! pgrep -x "vim"; then 
+    #if launched without specifying a filename...
+    if [ $# -lt 1 ]; then 
+        CWD=$(pwd)
+        file=$CWD/temporaryFile.txt
+    fi
+
+    # if no vim instance is running, or starting vim without specifiying a file
+    if [ ! pgrep -x "vim" ]; then 
+
+        #[DEPRECATED]: Line below may be deleted in a future commit!
         #i3-msg 'move container to workspace number " 3:vim "' > /dev/null 2>&1
 
-        #get current workspacw
+        #we want to switch to the workspace where we were before running vim
         CWS=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name')
 
         #disappear calling terminal
         i3-msg 'move scratchpad' >/dev/null 2>&1
 
-        #open vim in ws3
+        #focus the workspace where we want to launch vim
         i3-msg 'workspace " 3:vim "' > /dev/null 2>&1
 
-        #Open the file in a new terminal instance, witch a different config and detach
-        #from calling terminal
+        #[FIX]: Launch vim without a filename
+        if [ $# -lt 1 ]; then 
+            # command vim --servername $(command vim --serverlist | head -1) >/dev/null 2>&1 &
+        fi
+
+        #Launch vim with another terminal config and detach it from calling processs
         nohup alacritty \
             --config-file=/home/geeray/.config/alacritty/alacritty-nogap.yml \
             -e sh \
@@ -28,15 +42,14 @@ function f_vim_one_instance() {
         #wait for vim process to finish
         wait
 
+        #don't pollute screen
         clear
 
-        #switch to the orginial workspace
         i3-msg "workspace $CWS" >/dev/null 2>&1
         #make calling terminal appear again
         i3-msg "scratchpad show" >/dev/null 2>&1
         #maximize it on screen
         i3-msg "floating disable" >/dev/null 2>&1
-
 
         #exit
 
