30378f66b1c806e5fb552466729140f059cbf7a2
uestueab <abla.ue@web.de>
2021-06-07 12:13:12 +0200

    remove padding when launching vim, put terminal to the workspace its was before, open temp file if no filename was given

--- .zshrc.d/f_vim_one_instance.sh
+++ .zshrc.d/f_vim_one_instance.sh
@@ -2,30 +2,46 @@
 
 function f_vim_one_instance() {
 
-    sed -i "s/x: 20/x: 0/g" .config/alacritty/alacritty.yml
-    sed -i "s/y: 30/y: 0/g" .config/alacritty/alacritty.yml
+    #remove padding
+    sed -i "s/x: 20/x: 0/g" ~/.config/alacritty/alacritty.yml
+    sed -i "s/y: 30/y: 0/g" ~/.config/alacritty/alacritty.yml
 
-    #[ATTENTION]: This fixes spaces in filenames!
-    # but makes it impossible to launch vim with more than one file
-    #file="'$*'"
 
-    ##if launched without specifying a filename...
-    #if [ $# -lt 1 ]; then
-    #    CWD=$(pwd)
-    #    file=$CWD/temporaryFile.txt
-    #fi
-
-    #Vim is assigned to its specified workspace
+    #if there is no vim process running...
     if ! pgrep -x "vim"; then
+
+        #we want to switch to the workspace where we were before running vim
+        CWS=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name')
+
         i3-msg 'move container to workspace number " 3:vim "' > /dev/null 2>&1
         i3-msg 'workspace " 3:vim "' > /dev/null 2>&1   #switch to workspace
-        command vim --servername $(command vim --serverlist | head -1) --remote-silent "$@"
+
+        #open a temporary file if no filename was given
+        if [ $# -lt 1 ]; then
+            CWD=$(pwd)
+            TMPFILE=$(mktemp -u $CWD/tmp-XXXX.txt)
+            command vim --servername $(command vim --serverlist | head -1) --remote-silent "$TMPFILE"
+        #launchin vim with file(s)
+        else
+            command vim --servername $(command vim --serverlist | head -1) --remote-silent "$@"
+        fi
+
+        #wait for vim process to finish
+        wait
+
+
+        #go to the original workspace, in which we started vim
+        i3-msg "move container to workspace number $CWS" > /dev/null 2>&1
+        i3-msg "workspace $CWS" >/dev/null 2>&1
+
+    #there is a vim process running
     else
-        command vim --servername $(command vim --serverlist | head -1) --remote-silent "$@" &
+        command vim --servername $(command vim --serverlist | head -1) --remote-silent "$FILE" &
         i3-msg 'workspace " 3:vim "' > /dev/null 2>&1   #switch to workspace
         exit
     fi
 
-    sed -i "s/x: 0/x: 20/g" .config/alacritty/alacritty.yml
-    sed -i "s/y: 0/y: 30/g" .config/alacritty/alacritty.yml
+    #add padding
+    sed -i "s/x: 0/x: 20/g" ~/.config/alacritty/alacritty.yml
+    sed -i "s/y: 0/y: 30/g" ~/.config/alacritty/alacritty.yml
 }
