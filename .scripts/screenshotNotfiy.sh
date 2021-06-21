91e6a43dabfb4cec4e5e9e33829dbc8e9fb3a99b
uestueab <abla.ue@web.de>
2021-06-21 10:25:16 +0200

    screenshot options are reworked

--- .scripts/screenshotNotfiy.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-scrot -s ~/media/screenshots/shot-$(date '+%d-%b-%Y-%H-%M').png -q 100 -e 'xclip-selection
-c -t image/png < $f' && notify-send -i $HOME/media/wallpapers/icons/cute.png -u critical "Screenshot successful!" "Image located at ~/media/screenshots"
-
--- .scripts/screenshotNotfiyClipboard.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-scrot -s /tmp/screenshot-$(date '+%d-%b-%Y_%H-%M').png -e 'xclip -selection clipboard -t "image/png" < $f'
-notify-send -i $HOME/media/wallpapers/icons/cute.png -u critical "Screenshot successful!" "Image copied to clipboard"
-
