ed1d71e975aa291f63c0d2aa387bee6ab8241488
uestueab <abla.ue@web.de>
2021-07-17 13:04:14 +0200

    now copies to clipboard

--- .scripts/screenshotWindow.sh
+++ .scripts/screenshotWindow.sh
@@ -11,7 +11,7 @@ if pgrep -x "scrcpy"; then
     adb exec-out screencap -p > ~/media/screenshots/$fName.png
 
 else
-    if scrot -u ~/media/screenshots/$fName.png -e 'xclip-selection c -t image/png < $f' ; then
+    if scrot -u ~/media/screenshots/$fName.png -e 'xclip -selection clipboard -t "image/png" < $f' ; then
         notify-send -i $HOME/media/wallpapers/icons/cute.png -u critical "Screenshot successful!" "Image located at\n ~/media/screenshots/$fName.png"
     fi
 fi
