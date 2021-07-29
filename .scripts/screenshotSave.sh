e508d3ac318803324d30113aa627b8d7a5b14767
uestueab <abla.ue@web.de>
2021-07-29 21:49:17 +0200

    script is more efficient now

--- .scripts/screenshotSave.sh
+++ .scripts/screenshotSave.sh
@@ -6,28 +6,19 @@ fName=$(echo "" | rofi -dmenu -p "Enter filename ")
 
 #check empty string
 if [ -z "${fName// }" ]; then
-
-    #save screenshot with timestamp
-    scrot -s ~/media/screenshots/shot-$(date '+%d-%b-%Y-%H-%M').png \
-        -q 100 -e 'xclip-selection c -t image/png < $f'
-
-    #only notify if screenshot was taken
-    if [ $? -eq 0 ]; then
-        notify-send -i $HOME/media/wallpapers/icons/cute.png \
-            -u critical "Screenshot successful!" "Image located at ~/media/screenshots"
-    fi
-else
-    #save screenshot with the given filename
-    scrot -s ~/media/screenshots/$fName.png -e 'xclip-selection c -t image/png < $f'
-
-    #only notify if screenshot was taken
-    if [ $? -eq 0 ]; then
-        notify-send -i $HOME/media/wallpapers/icons/cute.png \
-            -u critical "Screenshot successful!" "Image located at\n ~/media/screenshots/$fName.png"
-    fi
+    fName="$(echo shot-$(date '+%d-%b-%Y-%H-%M'))"
 fi
 
+#take screenshot
+# * -s: selection
+# * -o: overwrite existing
+scrot \
+    -so ~/media/screenshots/$fName.png \
+    -q 100 \
+    -e 'xclip -selection clipboard -t image/png < $f'
 
-
-
-
+#only notify if screenshot was taken
+if [ $? -eq 0 ]; then
+    notify-send -i $HOME/media/wallpapers/icons/cute.png \
+        -u critical "Screenshot successful!" "Image located at ~/media/screenshots"
+fi
