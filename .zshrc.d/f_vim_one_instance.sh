da0112961ea8823ac60f8b32a47eff411292ebd7
uestueab <abla.ue@web.de>
2021-08-21 14:01:06 +0200

    tedious vim padding stays for now

--- .zshrc.d/f_vim_one_instance.sh
+++ .zshrc.d/f_vim_one_instance.sh
@@ -4,37 +4,27 @@ function f_vim_one_instance() {
 
 
     #remove padding
-    sed -i "s/x: 20/x: 0/g" ~/.config/alacritty/alacritty.yml
-    sed -i "s/y: 30/y: 0/g" ~/.config/alacritty/alacritty.yml
+    # sed -i "s/x: 20/x: 0/g" ~/.config/alacritty/alacritty.yml
+    # sed -i "s/y: 30/y: 0/g" ~/.config/alacritty/alacritty.yml
 
 
-
-
-    #if there is no vim process running...
+    #if there is no vim process already running...
     if ! pgrep -x "vim"; then
 
-        #we want to switch to the workspace where we were before running vim
+        #save current workspace pre vim launch
+        #so we can change to it right after! 
         CWS=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name')
 
         i3-msg 'move container to workspace number " 3:vim "' > /dev/null 2>&1
-        i3-msg 'workspace " 3:vim "' > /dev/null 2>&1   #switch to workspace
+        i3-msg 'workspace " 3:vim "' > /dev/null 2>&1   #move focus to vim workspace
 
-        #edit temporary file if no filename was given
-        if [ $# -lt 1 ]; then
-            CWD=$(pwd)
-            TMPFILE=$(mktemp -u $CWD/tmp-XXXX.txt)
-            command vim --servername $(command vim --serverlist | head -1) --remote-silent "$TMPFILE"
-
-        #launch vim with files
-        else
-            command vim --servername $(command vim --serverlist | head -1) --remote-silent "$@"
-        fi
+        command vim --servername $(command vim --serverlist | head -1) --remote-silent "$@"
 
         #wait for vim process to finish
         wait
 
 
-        #go to the original workspace, in which we started vim
+        #go back to the workspace pre vim launch..
         i3-msg "move container to workspace number $CWS" > /dev/null 2>&1
         i3-msg "workspace $CWS" >/dev/null 2>&1
 
@@ -46,6 +36,6 @@ function f_vim_one_instance() {
     fi
 
     #add padding
-    sed -i "s/x: 0/x: 20/g" ~/.config/alacritty/alacritty.yml
-    sed -i "s/y: 0/y: 30/g" ~/.config/alacritty/alacritty.yml
+    # sed -i "s/x: 0/x: 20/g" ~/.config/alacritty/alacritty.yml
+    # sed -i "s/y: 0/y: 30/g" ~/.config/alacritty/alacritty.yml
 }
