73d339fa9c6ce4d44f5b7f278fb368594c60e85a
uestueab <abla.ue@web.de>
2021-11-05 21:00:16 +0100

    edit the correct config

--- .scripts/vimEnter.sh
+++ .scripts/vimEnter.sh
@@ -4,5 +4,8 @@
 # sed -i "s/x: 20/x: 0/g" ~/.config/alacritty/alacritty.yml
 # sed -i "s/y: 30/y: 0/g" ~/.config/alacritty/alacritty.yml
 
+# get the file the config file points to
+configFile=$(readlink -f ~/.config/alacritty/alacritty.yml)
+
 #change background color for duration of vim 
-sed -i "s/background: '0x0a0b11'/background: '0x161821'/g" ~/.config/alacritty/alacritty.yml
+sed -i "s/background: '0x0a0b11'/background: '0x161821'/g" $configFile
--- .scripts/vimLeave.sh
+++ .scripts/vimLeave.sh
@@ -4,5 +4,8 @@
 # sed -i "s/x: 0/x: 20/g" ~/.config/alacritty/alacritty.yml
 # sed -i "s/y: 0/y: 30/g" ~/.config/alacritty/alacritty.yml
 
+# get the file the config file points to
+configFile=$(readlink -f ~/.config/alacritty/alacritty.yml)
+
 #change background color to terminal
-sed -i "s/background: '0x161821'/background: '0x0a0b11'/g" ~/.config/alacritty/alacritty.yml
+sed -i "s/background: '0x161821'/background: '0x0a0b11'/g" $configFile
