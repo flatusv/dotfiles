453b315b91e0f9c2a8b9d0a7cfdde9f3cae91771
uestueab <abla.ue@web.de>
2022-01-21 00:44:11 +0100

    update polybar

--- .scripts/mpcBlocks.sh
+++ .scripts/mpcBlocks.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if  [ "$(playerctl status)" == "Playing" ]
+if  [ "$(playerctl -s -p mpv,%any status)" == "Playing" ]
 then
-    currentTitle=$(playerctl metadata --format "{{ artist }} - {{ title }}" | sed 's/2020-.*:[0-9]\+$//')
-    echo "mpv: <span foreground='#dccdfb'><b>$currentTitle</b></span>"
+    currentTitle=$(playerctl -p mpv,%any metadata --format "{{ artist }} - {{ title }}" | sed 's/2020-.*:[0-9]\+$//')
+    echo "mpv: $currentTitle"
 fi
 
-echo -e "\n"
-# background color within border
-echo -e "#2b2b2b"
-# border color
-echo -e "#9180e8"
-
+# echo -e "\n"
+# # background color within border
+# echo -e "#2b2b2b"
+# # border color
+# echo -e "#9180e8"
+#
