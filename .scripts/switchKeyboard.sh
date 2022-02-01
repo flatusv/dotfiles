d6c7c4cbdd95bca1e35927a343bad6e67a885634
uestueab <abla.ue@web.de>
2022-02-01 15:25:45 +0100

    added notification function

--- .scripts/switchKeyboard.sh
+++ .scripts/switchKeyboard.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
+function notify(){
+    notify-send -i $HOME/media/wallpapers/icons/cute.png -u critical \
+        "Keyboard Changed!" "Now using: $(cat /tmp/kbdLayout.txt)"
+}
+
 #cycle between xmodmap and xkb, depending on which is currently in effect
 if grep -q "xmodmap" /tmp/kbdLayout.txt; then
     #reset the current layout
@@ -12,8 +17,7 @@ if grep -q "xmodmap" /tmp/kbdLayout.txt; then
     echo "xkb" >| /tmp/kbdLayout.txt
     echo "[KBD]: xkb"
 
-    notify-send -i $HOME/media/wallpapers/icons/cute.png -u critical \
-        "Changed Keyboard!" "Now using: $(cat /tmp/kbdLayout.txt)"
+    notify
 
     exit
 fi
@@ -30,8 +34,8 @@ if grep -q "xkb" /tmp/kbdLayout.txt; then
     echo "xmodmap" >| /tmp/kbdLayout.txt
     echo "[KBD]: xmodmap"
 
-    notify-send -i $HOME/media/wallpapers/icons/cute.png -u critical \
-        "Changed Keyboard!" "Now using: $(cat /tmp/kbdLayout.txt)"
+    notify
+
     exit
 fi
 
